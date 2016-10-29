1b2fc7962f6998540dfb4f92b4014877d3cc648f
Niranjan-B <nero.niranjan@gmail.com>
2016-10-29 16:28:48 -0700

    added autocomplete textview, image and modified the theme for the app

--- app/src/main/java/com/ninja/neighbours/MainActivity.java
+++ app/src/main/java/com/ninja/neighbours/MainActivity.java
@@ -2,12 +2,41 @@ package com.ninja.neighbours;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.AutoCompleteTextView;
+import android.widget.Toast;
+
+import butterknife.BindView;
+import butterknife.ButterKnife;
 
 public class MainActivity extends AppCompatActivity {
+
+    @BindView(R.id.autoCompleteTextView) AutoCompleteTextView mAutoCompleteTextView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        ButterKnife.bind(this);
+
+        initAutoCompleteTextView();
+    }
+
+    private void initAutoCompleteTextView() {
+        final String[] COUNTRIES = new String[] {"MardiGras", "MoonShine", "Trojan Living", "Ellandale", "2340"};
+
+        ArrayAdapter<String> adapter = new ArrayAdapter<String>(this,
+                android.R.layout.simple_dropdown_item_1line, COUNTRIES);
+        mAutoCompleteTextView.setAdapter(adapter);
+        mAutoCompleteTextView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+                Toast.makeText(MainActivity.this, "" + parent.getItemAtPosition(position), Toast.LENGTH_LONG).show();
+            }
+        });
     }
 }
