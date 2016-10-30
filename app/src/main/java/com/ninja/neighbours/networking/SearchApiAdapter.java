1b90786c29879ad3993d5a930bce81e573534045
Niranjan-B <nero.niranjan@gmail.com>
2016-10-30 08:56:12 -0700

    interface and retrofit adapter for search building (reverted from commit d860810ee035a14415b2172cd7890906d7a06a82)

--- app/src/main/java/com/ninja/neighbours/networking/SearchApiAdapter.java
+++ app/src/main/java/com/ninja/neighbours/networking/SearchApiAdapter.java
@@ -1,8 +1,19 @@
 package com.ninja.neighbours.networking;
 
+import retrofit2.GsonConverterFactory;
+import retrofit2.Retrofit;
+import retrofit2.RxJavaCallAdapterFactory;
+
 /**
  * Created by niranjanb on 30/10/16.
  */
 
 public class SearchApiAdapter {
+    public static Retrofit getSearchAdapter() {
+        return new Retrofit.Builder()
+                .baseUrl("https://myneighbourhood.herokuapp.com/")
+                .addConverterFactory(GsonConverterFactory.create())
+                .addCallAdapterFactory(RxJavaCallAdapterFactory.create())
+                .build();
+    }
 }
--- app/src/main/java/com/ninja/neighbours/networking/interfaces/SearchApiInterface.java
+++ app/src/main/java/com/ninja/neighbours/networking/interfaces/SearchApiInterface.java
@@ -1,8 +1,15 @@
 package com.ninja.neighbours.networking.interfaces;
 
+import com.ninja.neighbours.networking.DataModels.SearchModel;
+
+import retrofit2.http.GET;
+import rx.Observable;
+
 /**
  * Created by niranjanb on 30/10/16.
  */
 
 public interface SearchApiInterface {
+    @GET("api")
+    Observable<SearchModel> getSearchResults();
 }
