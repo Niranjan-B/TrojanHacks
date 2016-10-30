756dc894b75a38932342c2e407e4a5cf1865a392
Niranjan-B <nero.niranjan@gmail.com>
2016-10-29 22:00:35 -0700

    set-up tab's

--- app/src/main/java/com/ninja/neighbours/fragments/CarPoolingFragment.java
+++ app/src/main/java/com/ninja/neighbours/fragments/CarPoolingFragment.java
@@ -2,17 +2,26 @@ package com.ninja.neighbours.fragments;
 
 
 import android.os.Bundle;
+import android.support.design.widget.TabLayout;
 import android.support.v4.app.Fragment;
+import android.support.v4.view.ViewPager;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
 import com.ninja.neighbours.R;
+import com.ninja.neighbours.adapters.FragmentPagerAdapter;
+
+import butterknife.BindView;
+import butterknife.ButterKnife;
 
 /**
  * A simple {@link Fragment} subclass.
  */
 public class CarPoolingFragment extends Fragment {
+
+    @BindView(R.id.tablayout_carpooling) TabLayout mTabLayout;
+    @BindView(R.id.viewpager_carpooling) ViewPager mViewPager;
 
     public static CarPoolingFragment getNewInstance() {
         return new CarPoolingFragment();
@@ -26,8 +35,13 @@ public class CarPoolingFragment extends Fragment {
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_car_pooling, container, false);
+        View view = inflater.inflate(R.layout.fragment_car_pooling, container, false);
+        ButterKnife.bind(this, view);
+
+        mViewPager.setAdapter(new FragmentPagerAdapter(getActivity().getSupportFragmentManager()));
+        mTabLayout.setupWithViewPager(mViewPager);
+
+        return view;
     }
 
 }
