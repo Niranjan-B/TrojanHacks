8a0039f150e7ce2868534987c76888562e51d55a
Niranjan-B <nero.niranjan@gmail.com>
2016-10-29 22:00:03 -0700

    car pool fragment adapter

--- app/src/main/java/com/ninja/neighbours/adapters/FragmentPagerAdapter.java
+++ app/src/main/java/com/ninja/neighbours/adapters/FragmentPagerAdapter.java
@@ -1,8 +1,44 @@
 package com.ninja.neighbours.adapters;
 
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentManager;
+
+import com.ninja.neighbours.fragments.carpool_fragments.AvailableCarpoolsFragment;
+import com.ninja.neighbours.fragments.carpool_fragments.CarpoolRequestsFragment;
+import com.ninja.neighbours.fragments.carpool_fragments.MyCarpoolsFragment;
+
 /**
  * Created by niranjanb on 29/10/16.
  */
 
-public class FragmentPagerAdapter {
+public class FragmentPagerAdapter extends android.support.v4.app.FragmentPagerAdapter {
+    final int PAGE_COUNT = 3;
+    private String[] pageTitles = {"Available", "My Carpools", "Requests"};
+
+    public FragmentPagerAdapter(FragmentManager fm) {
+        super(fm);
+    }
+
+    @Override
+    public Fragment getItem(int position) {
+        if (position == 0) {
+            return AvailableCarpoolsFragment.getInstance();
+        } else if (position == 1) {
+            return MyCarpoolsFragment.getInstance();
+        } else if (position == 2) {
+            return CarpoolRequestsFragment.getInstance();
+        } else {
+            return null;
+        }
+    }
+
+    @Override
+    public CharSequence getPageTitle(int position) {
+        return pageTitles[position];
+    }
+
+    @Override
+    public int getCount() {
+        return PAGE_COUNT;
+    }
 }
