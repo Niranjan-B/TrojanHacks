60f8bc729b23a766f1e6b3b230ed4309e89a15a6
Niranjan-B <nero.niranjan@gmail.com>
2016-10-30 01:26:13 -0700

    added page change listener and changed fragmentManager to childFragmentManager

--- app/src/main/java/com/ninja/neighbours/fragments/CarPoolingFragment.java
+++ app/src/main/java/com/ninja/neighbours/fragments/CarPoolingFragment.java
@@ -5,6 +5,7 @@ import android.os.Bundle;
 import android.support.design.widget.TabLayout;
 import android.support.v4.app.Fragment;
 import android.support.v4.view.ViewPager;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -23,6 +24,7 @@ public class CarPoolingFragment extends Fragment {
     @BindView(R.id.tablayout_carpooling) TabLayout mTabLayout;
     @BindView(R.id.viewpager_carpooling) ViewPager mViewPager;
 
+
     public static CarPoolingFragment getNewInstance() {
         return new CarPoolingFragment();
     }
@@ -38,10 +40,25 @@ public class CarPoolingFragment extends Fragment {
         View view = inflater.inflate(R.layout.fragment_car_pooling, container, false);
         ButterKnife.bind(this, view);
 
-        mViewPager.setAdapter(new FragmentPagerAdapter(getActivity().getSupportFragmentManager()));
+        mViewPager.setAdapter(new FragmentPagerAdapter(getChildFragmentManager()));
         mTabLayout.setupWithViewPager(mViewPager);
+        mViewPager.addOnPageChangeListener(new ViewPager.OnPageChangeListener() {
+            @Override
+            public void onPageScrolled(int position, float positionOffset, int positionOffsetPixels) {
+
+            }
+
+            @Override
+            public void onPageSelected(int position) {
+
+            }
+
+            @Override
+            public void onPageScrollStateChanged(int state) {
+
+            }
+        });
 
         return view;
     }
-
 }
