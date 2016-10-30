d860810ee035a14415b2172cd7890906d7a06a82
Niranjan-B <nero.niranjan@gmail.com>
2016-10-30 08:55:43 -0700

    interface and retrofit adapter for search building

---
+++ app/src/main/java/com/ninja/neighbours/networking/SearchApiAdapter.java
@@ -0,0 +1,8 @@
+package com.ninja.neighbours.networking;
+
+/**
+ * Created by niranjanb on 30/10/16.
+ */
+
+public class SearchApiAdapter {
+}
---
+++ app/src/main/java/com/ninja/neighbours/networking/interfaces/SearchApiInterface.java
@@ -0,0 +1,8 @@
+package com.ninja.neighbours.networking.interfaces;
+
+/**
+ * Created by niranjanb on 30/10/16.
+ */
+
+public interface SearchApiInterface {
+}
